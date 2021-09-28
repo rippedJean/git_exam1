c680d0abe9f33e5572b816c4f612a2706bf8be49
kim <rlayjean0@gmail.com>
2021-09-28 09:29:08 +0900

    201914030-4

--- Hello.java
+++ Hello.java
@@ -3,9 +3,9 @@ package git_exam1;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("201914030");
-		System.out.println("김윤진");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 201914030");
+		System.out.println("이름: 김윤진");
 	}
 
 }
